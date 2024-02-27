3793038bd639983a45d2f9628b29ec923c45f953
Hadil Jilani <jilanihadil@gmail.com>
2024-02-27 09:09:35 +0100

    created palindrome app

---
+++ script.js
@@ -0,0 +1,34 @@
+function isPalindrome(text) {
+
+    text = text.toLowerCase().replace(/[^0-9a-z]/gi, '');
+    // Check if the string is a palindrome
+    let reversedText = text.split("").reverse().join('');
+    return (reversedText === text)
+}
+
+function showResult (res) {
+    let resultZone = document.getElementById('result')
+    resultZone.innerHTML = res;
+}
+
+function startGame() {
+    let btnCheck = document.getElementById("check-btn")
+    let textInput = document.getElementById("text-input")
+    
+    let result = false
+    btnCheck.addEventListener("click", () => {
+        if (textInput.value === "") {
+            showResult("Please enter some text.")
+        }
+        else {
+            result = isPalindrome(textInput.value)
+        if (result) {
+            showResult("This is a palindrome!");
+        } else {
+            showResult('This is not a palindrome')
+        }
+        }
+        
+    })
+}
+startGame()
